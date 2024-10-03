a98e219732995ea9c1ca8cae4c72b7cbef1c8eee
YuyaTokutake1223 <yuya-tokutake@a-hub.net>
2024-10-03 14:51:43 +0900

    usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,            presence: true
+  validates :last_name,           presence: true
+  validates :last_name_furigana,  presence: true
+  validates :first_name,          presence: true
+  validates :first_name_furigana, presence: true
+  validates :birthday,            presence: true
+
+# 下記のモデルはまだ作成していないため保留（10/03）
+  # has_many :products
+  # has_many :purchase_records
+  
 end
