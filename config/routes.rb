d1e88f0d098a379ccd2f755d45388fc2821a77f3
YuyaTokutake1223 <yuya-tokutake@a-hub.net>
2024-10-04 18:40:01 +0900

    コントローラーの作成及びルーティングの設定
    ルートパスをitems#indexに変更した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  
-  root "orders#index"
+  root "items#index"
+  resources :items, only: [:index, :new, :create, :edit, :update, :show]
+  resources :orders, only: :index
 end
