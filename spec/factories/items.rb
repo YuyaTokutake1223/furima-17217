0eb7c543dfd6120499f6d991608c2022b94a60a8
YuyaTokutake1223 <yuya-tokutake@a-hub.net>
2024-10-05 19:26:19 +0900

    単体テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    item_name { Faker::Internet.name }
+    description { Faker::Lorem.paragraph }
+    category_id { rand(2..11) }
+    condition_id { rand(2..7) }
+    shipping_cost_id { rand(2..3) }
+    prefecture_id { rand(2..48) }
+    shipping_duration_id { rand(2..4) }
+    sales_price { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open(Rails.root.join('public/image/test_image.png')), filename: 'test_image.png')
+    end
   end
 end
