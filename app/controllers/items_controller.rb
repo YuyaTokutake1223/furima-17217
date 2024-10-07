41c8514efbfead77c5dab73bd2e2bbf4f021e2a5
YuyaTokutake1223 <yuya-tokutake@a-hub.net>
2024-10-07 19:26:46 +0900

    updateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,11 @@ class ItemsController < ApplicationController
   def edit
   end
   
-  # 先の段階で実装するため一旦コメントアウト（10/07）
-  # def update
-  # end
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to root_path
+  end
   
 
 
