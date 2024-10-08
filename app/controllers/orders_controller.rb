1687bab7f96206d69b6431f73f0c4905c6760fda
YuyaTokutake1223 <yuya-tokutake@a-hub.net>
2024-10-08 23:47:10 +0900

    ordersテーブルへの保存設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,27 @@
 class OrdersController < ApplicationController
   def new
     @item = Item.find(params[:item_id])
+    @order = Order.new
   end
 
   def create
+    binding.pry
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :new, status: :unprocessable_entity
+    end   
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit().merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
+
