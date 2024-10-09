8db176d03be7893378c3c4af0e9819791b23e471
YuyaTokutake1223 <yuya-tokutake@a-hub.net>
2024-10-09 19:59:54 +0900

    購入済商品に対する表示及び遷移画面の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,6 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
-    
   end
 
   def new
@@ -25,6 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
   
   def update
